9c54bd523cf4ea664828a92c7de8a510c1763ee2
artsmoura <mourarthur@outlook.com>
2025-06-04 18:01:19 -0300

    ajusta tela do app para utilizar componentes de tab e stack

--- src/App.tsx
+++ src/App.tsx
@@ -9,46 +9,16 @@ import PesquisaHeader from './components/pesquisaHeader';
 import ResultadoScreen from './app/resultadoScreen';
 import { MaisNoticiasScreen } from './app/maisNoticias';
 import categoriaNomes from './constants/categoriaNomes';
-
-const Stack = createNativeStackNavigator<RootStackParamList>()
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FavoritosScreen from './app/favoritosScreen';
+import { Ionicons } from '@expo/vector-icons';
+import TabNavigation from './components/tabNavigation';
 
 export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerStyle: { 
-              backgroundColor: "#0072AE" 
-            },
-            headerTintColor: "#fff",
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            },
-            headerRight: () => <PesquisaHeader />,
-            headerTitle: "Notícias",
-          }}
-        >
-          <Stack.Screen 
-            name="Home" component={HomeScreen}
-            options={{
-              headerTitle: 'Notícias',
-              headerRight: () => <PesquisaHeader />,
-            }} 
-          />
-          <Stack.Screen name="Detalhes" component={DetalheScreen} />
-          <Stack.Screen name="Resultados" component={ResultadoScreen} />
-          <Stack.Screen 
-            name="MaisNoticias"
-            component={MaisNoticiasScreen}
-            options={({ route }) => {
-              const categoria = route.params?.categoria;
-              return {
-                headerTitle: categoria ? categoriaNomes[categoria] || 'Mais Notícias' : 'Mais Notícias',
-              };
-            }}
-          />
-        </Stack.Navigator>
+        <TabNavigation />
       </NavigationContainer>
     </Provider>
   );
