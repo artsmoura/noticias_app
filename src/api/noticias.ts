fbec8298fbaf963271497452d6a8b49ab2bb3385
artsmoura <mourarthur@outlook.com>
2025-06-05 16:00:39 -0300

    Ajusta o axios e achamada da API para controlar melhor os parametros recebidos e tambem ter um log para casos de erro

--- src/api/noticias.ts
+++ src/api/noticias.ts
@@ -4,17 +4,34 @@ import { NoticiasApiResponse } from 'src/types/noticias';
 
 
 export const getNoticias = async (params: { country?: string; category?: string; q?: string; page?: number }): Promise<NoticiasApiResponse> => {
-  const baseURL = params.q ? 'https://newsapi.org/v2/everything' : 'https://newsapi.org/v2/top-headlines';
-  const response = await axios.get(baseURL, {
-    params: {
-      apiKey: EXPO_PUBLIC_API_KEY,
-      pageSize: 10,
-      page: params.page || 1,
-      ...params
+  const isSearch = !!params.q;
+  const baseURL = isSearch ? 'https://newsapi.org/v2/everything' : 'https://newsapi.org/v2/top-headlines';
+
+  const queryParams: any = {
+    apiKey: EXPO_PUBLIC_API_KEY,
+    pageSize: 10,
+    page: params.page || 1
+  };
+
+  if (isSearch) {
+    queryParams.q = params.q;
+  } else {
+    queryParams.country = params.country || 'us';
+    if (params.category) {
+      queryParams.category = params.category;
     }
-  })
-  return {
-    totalResults: response.data.totalResults,
-    articles: response.data.articles
+  }
+
+  try {
+    const response = await axios.get(baseURL, { params: queryParams });
+    console.log('Resposta da API:', response.data);
+
+    return {
+      totalResults: response.data.totalResults,
+      articles: response.data.articles
+    }
+  } catch (error: any) {
+    console.error("Erro na requisição:", error?.response?.data || error.message || error);
+    throw error;
   }
 }
